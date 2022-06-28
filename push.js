2a7295b3f1918890b4d18d3bca4da47aad1b23d8
d0iloppa <kdi3939@gmail.com>
2022-06-28 15:14:48 +0900

    test

--- push.js
+++ push.js
@@ -19,6 +19,7 @@ let sub = {
         }
 }
 
-let sub2 = {"endpoint":"https://fcm.googleapis.com/fcm/send/diBDLGU-No8:APA91bECrD5MQWGizi_0mBujmgZuc-iyPOA1Eudht-fdwDhaq3MXgRvyzDdECv7fc7PCkKRLgDVDi2lSq_phzjdHRII3kbBLprDR72TwLRUKDG0q6Yuz-2c14_Oj2yuqjOQpwdVvLjm-","expirationTime":null,"keys":{"p256dh":"BM6TiSMRaan6ZZzx54t-Pqx1VdYwfnlxqb-QKSzYzHteKShVPX_OANxxVUdoP_xrPdXNbVl0zT__ssMUvxk3ckE","auth":"nIF_O5vRpEx5xFXLvCCizw"}};
+let sub2 = {"endpoint":"https://fcm.googleapis.com/fcm/send/d56-iiAfdJE:APA91bEGg62lpeDeBXP2ziV2sgrNVrWU8uGVu34kuFaVNyvOZHYaMGL0BKzouiGRXLNhgOkjH89btwnMmrZqH1ZoKUcq4TcKQqPuBRM3egwTcdut0ynIWVZ5jjMrNnmJVXug5T4h4303","expirationTime":null,"keys":{"p256dh":"BDRHyIS6pgVHb4tE5iKcEFWm2mkc9g1waJ2gH14OmClvVUell6F6XdGz1SYvHtsconWrqCVFRnMCq6E1uHx7EoU","auth":"T75pmprvMmWaQpEzxNiM6A"}}
+let sub3 = {"endpoint":"https://wns2-pn1p.notify.windows.com/w/?token=BQYAAAAVp00z0DLC9Vompq5PKAT1ypv4PDquyeVaRsSk1RgC7QBRvZGRF6TXwPeGtwNjrxdI0InBc4dBlHXvuYMNOo3S2NFLaxeFeATOYIgF3Rmf6hfV9qb3AE79JiNuoDA%2bXCpqw8blhMeWz7GdTxk036bHu5yzE8h5NEn1pyW9TfIOyoSblGd2np9iSGx%2fu94MYU14spbYJUlqBIsRV1IkZS5zFImuIEaByudEE%2bSHUoLiulvE03ZKRMPmOryLY6qiTSnpMEM8996m2wQB8oc3IKuRbzYg5hka6Ev0tpwkE%2f%2bgvpxwBAOvg7RsJ%2b6OIIhIdjRQOxsp66qM7jCBS00ksk2cp8ikDVfbiQ9GNkjOB1qz5A%3d%3d","expirationTime":null,"keys":{"p256dh":"BOkoF8SggPWZ_w2cxkd6GLh3le256wHkakkvHxOlkGJmrB5_cZliryy7vVi0ixFNTz7A4vg2XcEURs52R2-f9dM","auth":"eW6kHhkAZyYb9voPVHECRw"}};
 
 push.sendNotification(sub2,'test message');
