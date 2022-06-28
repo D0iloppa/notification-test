ba5c6a5101741dccc8094446414a0078ebb9cf92
d0iloppa <kdi3939@gmail.com>
2022-06-28 14:38:28 +0900

    fix

--- push.js
+++ push.js
@@ -10,7 +10,14 @@ push.setVapidDetails('mailto:kdi3939@naver.com' , vapidKeys.publicKey , vapidKey
 
 
 
-let sub = {};
+let sub = {
+    endpoint:"https://fcm.googleapis.com/fcm/send/dyyXRVqFa0I:APA91bFX0-dtNPkXMq8pvX6AKATfxSrRspBhqhZ8r-Wz2LpDhqdJOvb9TCNhrpsDVV8PDfxI_vCYlU0pzu-JG3ovuZYwwmYjYAbf2aJRMLqEks2ZglegeX8AxZ7ox5ITOVZwBumOh9M_",
+    expirationTime:null,
+    keys:{
+            p256dh:"BAG13FkUX64yCezV4owm-iJWYbUzS9cEJ1A34uoNYCCRD7M4QdFafE-o70L8CvIg4ZIKYepccdXSNcYBKVu1ndo",
+            auth:"deS-6Y2y0pBCe73F61sKkQ"
+        }
+}
 
 
 push.sendNotification(sub,'test message');
--- sw.js
+++ sw.js
@@ -1,3 +1,34 @@
+self.addEventListener('push' , (e) => {
+    var options = {
+        body : "This notification was generated from a Doil's server ",
+        icon : "images/example.png",
+        vibrate : [100, 50, 100],
+        data : {
+            dateOfArrival : Date.now(),
+            primaryKey : '2'
+        },
+        actions:[
+            {
+                action : 'explore',
+                title : 'Explore this new world',
+                icon : 'images/checkmark.png'
+            },
+            {
+                action : 'close',
+                title : 'Close',
+                icon : 'images/xmark.png'
+            },
+        ]
+    }
+
+    e.waitUntil(
+        self.registration.sendNotification('Hello world!',options)
+    );
+    
+});
+
+/*
 self.addEventListener('push' , () => {
     self.registration.sendNotification('test Message',{});
 });
+*/
